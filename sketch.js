cbbbb0fa797d2b941b58e0a34b043253c0d9657d
ProfVine <viniciusvideira@prof.educacao.sp.gov.br>
2024-08-19 07:46:40 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,57 +1,40 @@
-
-let xBolinha = 100;
+//variáveis da bolinha
+let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 22;
+let diametro = 15;
 let raio = diametro / 2;
 
+//velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
-let xRaquete = 5;
-let yRaquete = 150;
-let raqueteComprimento = 10;
-let raqueteAltura = 90;
-
 function setup() {
   createCanvas(600, 400);
+  background (0)
 }
-
 function draw() {
-  background(0);
-  mostraBolinha();
-  movimentaBolinha();
-  verificaColisaoBorda();
-  mostraRaquete();
-  movimentaMinhaRaquete();
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
 }
 
 function mostraBolinha() {
-  circle(xBolinha, yBolinha, diametro);
+    circle(xBolinha, yBolinha, diametro)
 }
 
 function movimentaBolinha() {
-  xBolinha += velocidadeXBolinha;
-  yBolinha += velocidadeYBolinha;
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
 }
 
 function verificaColisaoBorda() {
-  if (xBolinha + raio > width || xBolinha - raio < 0) {
-    velocidadeXBolinha *= -1;
-  }
-  if (yBolinha + raio > height || yBolinha - raio < 0) {
-    velocidadeYBolinha *= -1;
-  }
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
+        velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
+        velocidadeYBolinha *= -1;
+    }
 }
 
-function mostraRaquete() {
-  rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
-}
 
-function movimentaMinhaRaquete() {
-  if(keyIsDown(UP_ARROW)) {
-    yRaquete -= 10;
-  }
-  if(keyIsDown(DOWN_ARROW)) {
-    yRaquete += 10;
-  }
-}
